c8fc6980c7619b138d1532836d066f7e5dc960e6
Renato Salvadori <renatosalvadori@MacBook-Pro.local>
2026-04-10 16:37:49 -0300

    fix TESTE  DE ENAUL RESEND BUG FIX

--- utils/email_sender.py
+++ utils/email_sender.py
@@ -1,35 +1,42 @@
+# utils/email_sender.py
+
 import requests
+import base64
 
 RESEND_API_KEY = "re_jBBWhuEX_JAGjT36prrG1kbPpKeBabd46"
-
 
 def send_email(to_email, subject, body, attachment_path=None):
 
     url = "https://api.resend.com/emails"
 
     headers = {
-        "Authorization": f"Bearer {RESEND_API_KEY}"
+        "Authorization": f"Bearer {RESEND_API_KEY}",
+        "Content-Type": "application/json"
     }
 
-    data = {
+    payload = {
         "from": "RS Attack Surface <onboarding@resend.dev>",
-        "to": to_email,
+        "to": [to_email],
         "subject": subject,
         "text": body
     }
 
-    files = None
-
     if attachment_path:
-        files = [
-            ("attachments", open(attachment_path, "rb"))
+        with open(attachment_path, "rb") as f:
+            encoded = base64.b64encode(f.read()).decode()
+
+        payload["attachments"] = [
+            {
+                "filename": "relatorio.pdf",
+                "content": encoded
+            }
         ]
 
     response = requests.post(
         url,
         headers=headers,
-        data=data,
-        files=files
+        json=payload
     )
 
-    print("RESEND RESPONSE:", response.text)
+    print("RESEND STATUS:", response.status_code)
+    print("RESEND BODY:", response.text)
