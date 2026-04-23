296c8fa45e9efe8bb16022cd99fc3e6b3f3ffa6a
Renato Salvadori <renatosalvadori@MacBook-Pro.local>
2026-04-23 10:39:09 -0300

    mudanca do nuclei para reduzir timeout 3

--- scan/nuclei_analyzer.py
+++ scan/nuclei_analyzer.py
@@ -11,7 +11,7 @@ def run_nuclei(domain: str) -> list[dict]:
         nuclei_path,
         "-u", target,
 
-        "-id", "http-missing-security-headers,default-login",
+        "-id", "default-login",
         "-exclude-tags", "dos,fuzz,bruteforce,token,secret,creds,auth-bypass,global-matchers",
 
         "-rl", "2",
